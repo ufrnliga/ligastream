ff68b6f5f8a9164636a6099ad399431dd4531ba5
Vinicio <almeida.vinicio@gmail.com>
2024-04-26 15:14:31 -0300

    dois

--- liga.py
+++ liga.py
@@ -5,8 +5,6 @@ import pandas as pd
 import yfinance as yf
 import plotly.express as px
 import plotly.graph_objects as go
-import tradingcomdados
-from tradingcomdados import alternative_data as ad
 
 # Título da página
 st.title('Liga de Investimentos da UFRN')
@@ -58,22 +56,25 @@ if opcao_grafico == 'Volatilidades no Ibovespa':
     st.markdown("""Atualizado diariamente.""")
     st.markdown('---')
     
-    ad.index_composition('ibov')
-    codigos = ad.index_composition('ibov')['cod']
-    codsa = codigos + '.SA'
-    # Criar um DataFrame vazio para armazenar os dados baixados
-    dados_acoes = pd.DataFrame()
-    # Baixar os dados para cada ação
-    for cod in codsa:
-        try:
-            # Baixar os dados utilizando yfinance e adicionar à DataFrame
-            dados_acao = yf.download(cod, start='2024-01-01')['Adj Close'].to_frame()
-            dados_acoes = pd.concat([dados_acoes, dados_acao.rename(columns={'Adj Close': cod})], axis=1)
-        except Exception as e:
-            print(f"Erro ao baixar dados para {codsa}: {e}")
+    assets = [
+                "ABEV3.SA", "ALPA4.SA", "ARZZ3.SA", "ASAI3.SA", "AZUL4.SA", "B3SA3.SA", "BBAS3.SA", 
+                "BBDC3.SA", "BBDC4.SA", "BBSE3.SA", "BEEF3.SA", "BPAC11.SA", "BRAP4.SA", "BRFS3.SA", "BRKM5.SA", 
+                "CASH3.SA", "CCRO3.SA", "CIEL3.SA", "CMIG4.SA", "CMIN3.SA", "COGN3.SA", "CPFE3.SA", "CPLE6.SA", 
+                "CRFB3.SA", "CSAN3.SA", "CSNA3.SA", "CVCB3.SA", "CYRE3.SA", "DXCO3.SA", "EGIE3.SA", "ELET3.SA", 
+                "ELET6.SA", "EMBR3.SA", "ENEV3.SA", "ENGI11.SA", "EQTL3.SA", "EZTC3.SA", "FLRY3.SA", 
+                "GGBR4.SA", "GOAU4.SA", "GOLL4.SA", "HAPV3.SA", "HYPE3.SA", "IGTI11.SA", "IRBR3.SA", "ITSA4.SA", 
+                "ITUB4.SA", "JBSS3.SA", "KLBN11.SA", "LREN3.SA", "LWSA3.SA", "MGLU3.SA", "MRFG3.SA", "MRVE3.SA", 
+                "MULT3.SA", "NTCO3.SA", "PCAR3.SA", "PETR4.SA", "PETZ3.SA", "PRIO3.SA", "RADL3.SA", 
+                "RAIL3.SA", "RAIZ4.SA", "RDOR3.SA", "RENT3.SA", "RRRP3.SA", "SANB11.SA", "SBSP3.SA", "SLCE3.SA", 
+                "SMTO3.SA", "SOMA3.SA", "SUZB3.SA", "TAEE11.SA", "TIMS3.SA", "TOTS3.SA", "UGPA3.SA", "USIM5.SA", 
+                "VALE3.SA", "VBBR3.SA", "BHIA3.SA", "VIVT3.SA", "WEGE3.SA", "YDUQ3.SA"
+            ]
 
-    retornos_diarios = dados_acoes.pct_change().dropna()
-    desvio_padrao_diario = retornos_diarios.std()
+    #download data
+    data = yf.download(assets, start='2024-01-01')
+    # compute non-compounding, daily returns
+    returns = data['Adj Close'].pct_change().dropna()    
+    desvio_padrao_diario = returns.std()
     desvio_padrao_diario.index = desvio_padrao_diario.index.str.slice(stop = -3)
 
     # Plotar os desvios padrão diários
